dbf07acc5fda59721fb97f5bfc8c317ee29af49d
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-11-29 22:00:36 +0100

    refactored all component tests to e2e tests

--- cypress/component/App.cy.tsx
+++
@@ -1,8 +0,0 @@
-import App from '../../src/App';
-
-describe('App.cy.ts', () => {
-  it('hello world', () => {
-    cy.mount(<App />);
-    cy.getBySelector('hello-world-p').contains('Hello World');
-  });
-});
