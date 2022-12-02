0d9a0da1e6ab3adde5c4fb608a8ac49927a6460a
justyna <towarnickaj@gmail.com>
2022-12-02 08:17:14 +0100

    created mui theme

---
+++ src/CustomTheme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const colorTheme = createTheme({
+  palette: {
+    background: {
+      default: '#F5F5F5',
+    },
+    primary: {
+      main: '#0044B0',
+    },
+    secondary: {
+      main: '#D9D9D9',
+    },
+  },
+});
+
+export default colorTheme;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import colorTheme from './CustomTheme';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={colorTheme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
