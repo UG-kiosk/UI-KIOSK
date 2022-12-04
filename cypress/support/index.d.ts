6c0c38e6067b056d68a9794ac0e04a592f406eb9
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-12-04 22:28:43 +0100

    fix

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,5 @@
 declare namespace Cypress {
   interface Chainable {
-    getBySelector(selector: string): Chainable<Element>;
+    getBySelector(selector: string, ...args: any[]): typeof getBySelector;
   }
 }
