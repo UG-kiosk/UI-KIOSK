a4c909c39211283c4533551f2368fb02b587712d
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2022-12-12 20:24:12 +0100

    create i18n instance

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,17 @@ import colorTheme from './theme';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { ThemeProvider as EmotionThemeProvier } from '@emotion/react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MUIThemeProvider theme={colorTheme}>
       <EmotionThemeProvier theme={colorTheme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
+        <I18nextProvider i18n={i18n}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </I18nextProvider>
       </EmotionThemeProvier>
     </MUIThemeProvider>
   </React.StrictMode>,
