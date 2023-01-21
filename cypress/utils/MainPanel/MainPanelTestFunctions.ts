4a40732b596c8e3d7929804cc820648934dbb97f
Jakub Zapiórkowski <jakub.zapiorkowski@palturai.com>
2023-01-22 00:12:20 +0100

    test main panel navigation link

--- cypress/utils/MainPanel/MainPanelTestFunctions.ts
+++ cypress/utils/MainPanel/MainPanelTestFunctions.ts
@@ -1,5 +1,27 @@
 export class MainPanelTestFunctions {
-  getMainPanel = () => cy.getBySelector('main-panel');
+  private getMainPanel = () => cy.getBySelector('main-panel');
 
-  getTile = (selector: string) => cy.getBySelector(selector);
+  private getTile = () => cy.getBySelector('tile');
+
+  private testMainPanelContent = () => {
+    this.getMainPanel().should('exist');
+    this.getTile().should('have.length', 7);
+  };
+
+  testMainPanelContentPL = () => {
+    this.testMainPanelContent();
+    this.getTile().eq(1).should('have.text', 'kierunki studiów');
+  };
+
+  testMainPanelContentEN = () => {
+    this.testMainPanelContent();
+    this.getTile().eq(1).should('have.text', 'majors');
+  };
+
+  testOnClickNavigations = () => {
+    this.getTile().eq(1).click();
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/majors');
+    });
+  };
 }
