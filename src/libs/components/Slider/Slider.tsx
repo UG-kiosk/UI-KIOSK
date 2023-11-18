bcdeac09260915a3dd3f638c9410a818e71a0116
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-11-18 12:47:03 +0100

    fix slider

--- src/libs/components/Slider/Slider.tsx
+++ src/libs/components/Slider/Slider.tsx
@@ -5,38 +5,31 @@ interface SliderProps {
   children?: ReactNode;
   className?: string;
   gap?: number;
-  ml?: number;
 }
 
 const StyledSlider = styled('div', {
   shouldForwardProp: prop => prop !== 'gap',
 })<SliderProps>`
   gap: ${({ gap }) => (gap ? `${gap}px` : '10px')};
-  overflow-x: scroll;
+  overflow: auto;
+  padding-inline: calc((100vw - 100%) / 2);
+  margin-left: calc((100vw - 100%) / -2);
   display: flex;
   -ms-overflow-style: none;
   scrollbar-width: none;
-  left: 50%;
-  right: 50%;
-  margin-left: -50vw;
-  margin-right: -50vw;
-  width: 100vw;
-  position: relative;
+
+  width: calc(100% + (100vw - 100%));
 
   overflow-scrolling: touch;
   -webkit-overflow-scrolling: touch;
-
-  & > img:first-child {
-    margin-left: ${({ ml }) => (ml ? `${ml}px` : '0px')};
-  }
 
   &::-webkit-scrollbar {
     display: none;
   }
 `;
 
-export const Slider = ({ children, className, gap, ml }: SliderProps) => (
-  <StyledSlider gap={gap} className={className} ml={ml}>
+export const Slider = ({ children, className, gap }: SliderProps) => (
+  <StyledSlider gap={gap} className={className}>
     {children}
   </StyledSlider>
 );
